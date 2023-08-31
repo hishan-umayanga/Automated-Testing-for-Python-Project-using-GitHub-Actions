b15faf16c4b33c4be34c44d0d248eb558b0c1800
hishan <hishanumayanga@gmail.com>
2023-09-01 00:07:00 +0530

    divide name change

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -30,7 +30,7 @@ def sub(num1, num2):
 def mul(num1, num2):
     return num1 * num2
 
-def divide(num1, num2):
+def divi(num1, num2):
     return num1 / num2 if num2 != 0 else None
 
 # Main calculator logic
